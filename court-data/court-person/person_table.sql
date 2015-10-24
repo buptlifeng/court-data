76dfcc4231b2b7616de28099767c90b96d49d87a
lex <lifeng719@126.com>
2015-10-24 13:11:02 +0800

    modify sql comment

--- court-data/court-person/person_table.sql
+++ court-data/court-person/person_table.sql
@@ -1,23 +1,24 @@
 #coding=utf8
-create table person_court_info(
+drop table if exists person_court_info;
+CREATE TABLE person_court_info(
     id integer primary key autoincrement,
-    case_id text comment '�����������',
-    iname text comment '��ִ��������/����',
-    sexy text comment '�Ա�',
-    age TINYINT comment '����',
-    cardNum text comment '���֤����/��֯��������',
-    courtName text comment 'ִ�з�Ժ',
-    areaName text comment 'ʡ��',
-    gistId text comment 'ִ�������ĺ�',
-    regDate text comment '����ʱ��',
-    caseCode text comment '����',
-    gistUnit text comment '����ִ�����ݵ�λ',
-    duty text comment '��Ч��������ȷ��������',
-    performance text comment '��ִ���˵��������',
-	performedPart text comment '�Ѿ����в���',
-	unperformPart text comment 'δ���в���',
-    disruptTypeName text comment 'ʧ�ű�ִ������Ϊ��������',
-    publishDate text comment '����ʱ��',
-    partyTypeName integer comment '��ע����',
-    remark text comment '��ע'
-    );
+    case_id text comment '案件索引编号',
+    iname text comment '被执行人姓名/名称',
+    sexy text comment '性别',
+    age TINYINT comment '年龄',
+    cardNum text comment '身份证号码/组织机构代码',
+    courtName text comment '执行法院',
+    areaName text comment '省份',
+    gistId text comment '执行依据文号',
+    regDate text comment '立案时间',
+    caseCode text comment '案号',
+    gistUnit text comment '作出执行依据单位',
+    duty text comment '生效法律文书确定的义务',
+    performance text comment '被执行人的履行情况',
+    performedPart text comment '已经履行部分',
+    unperformPart text comment '未履行部分',
+    disruptTypeName text comment '失信被执行人行为具体情形',
+    publishDate text comment '发布时间',
+    partyTypeName integer comment '关注次数',
+    remark text comment '备注'
+)engine=innodb default charset=utf8;
